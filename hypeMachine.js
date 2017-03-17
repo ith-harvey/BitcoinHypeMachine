b83a01679f5c94d7f4d94c2d98ff2fb1ecdcf4d6
Ian Harvey <ith.harvey@gmail.com>
2017-03-17 07:10:01 -0700

    updated CSS looks 8/10 ready to present

--- hypeMachine.js
+++ hypeMachine.js
@@ -4,6 +4,7 @@ let watsonScr = 0
 let total = 0
 let ultimateWatsonObj = new Object()
 let finalWatsonObj
+let myprintNumberofRequestCalls = 0
 
 
 // user & pass for Watson API
@@ -36,7 +37,7 @@ function bitBlockRefCheck(text) {
 }
 
 function setHeader(xhr) {
-    xhr.setRequestHeader('authorization', 'OAuth oauth_consumer_key="DC0sePOBbQ8bYdC8r4Smg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1489633016",oauth_nonce="1581578900",oauth_version="1.0",oauth_token="841456239800283136-aiCkdGp8kqWhQ4Lg8wcdxxhkXhnF8ta",oauth_signature="4O8Hl5F9r9525xAPQ4mjn9XDJEQ%3D"');
+    xhr.setRequestHeader('authorization', 'OAuth oauth_consumer_key="DC0sePOBbQ8bYdC8r4Smg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1489754200",oauth_nonce="1303887026",oauth_version="1.0",oauth_token="841456239800283136-aiCkdGp8kqWhQ4Lg8wcdxxhkXhnF8ta",oauth_signature="BeM6bOKvz%2BRXPRwbndsPOlI0FpQ%3D"');
 }
 
 // initiate twitter GET Request
@@ -62,10 +63,10 @@ $.ajax({
             return bitBlockRefCheck(element['text'])
         }
     })
-
+    console.log('response1 length', response.length);
 
     function setHeader(xhr) {
-        xhr.setRequestHeader('authorization', 'OAuth oauth_consumer_key="DC0sePOBbQ8bYdC8r4Smg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1489696891",oauth_nonce="2096730746",oauth_version="1.0",oauth_token="841456239800283136-aiCkdGp8kqWhQ4Lg8wcdxxhkXhnF8ta",oauth_signature="Wajq3chNjGwz0rvA10xZ77Vljzc%3D"');
+        xhr.setRequestHeader('authorization', 'OAuth oauth_consumer_key="DC0sePOBbQ8bYdC8r4Smg",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1489754118",oauth_nonce="886683261",oauth_version="1.0",oauth_token="841456239800283136-aiCkdGp8kqWhQ4Lg8wcdxxhkXhnF8ta",oauth_signature="B82cnht4Rbfx4qy%2FFWPpmCF6SUU%3D"');
     }
 
     // initiate twitter GET Request
@@ -91,8 +92,12 @@ $.ajax({
                 return bitBlockRefCheck(element['text'])
             }
         })
+
         console.log('response2 length', response2.length);
         response = response.concat(response2)
+        $('#total-tweets-pulled').fadeIn('slow')
+        $('#total-tweets-pulled').append('<p>' + response.length + '</p>')
+        console.log(response);
 
         var promises = response.map(tweet => {
             return $.ajax({
@@ -104,8 +109,11 @@ $.ajax({
             }).then(function(result) {
                 // console.log('result from watson', result);
                 // append your shit
+
                 $(".twitt-scroller").append('<div class="tweet-box col-xs-12">' +
-                    '<div class="col-xs-10 turn-off-padding">' + '<div class="tweet-txt">' + '<img class="thumbnail img-float-text-wrap" src=' + tweet['user']['profile_image_url'] + '>' + tweet['text'] + '</div>' + '</div>' + '<div class="col-xs-2 turn-off-padding">' + '<p>' + result['sentiment']['document']['label'] + '</p>' + '<p>' + result["sentiment"]["document"]["score"] + '</p>' + '</div>' + '</div>')
+                    '<div class="col-xs-10 turn-off-padding">' + '<img class="col-xs-2 thumbnail img-float-text-wrap" src=' + tweet['user']['profile_image_url'] + '>'+ '<h6 class=" username col-xs-8">' + tweet['user']['name'] +'</h6>' + '</div>' + '<div class="col-xs-2 turn-off-padding">' + '<p>' + result['sentiment']['document']['label'] + '</p>' + '<p>' + result["sentiment"]["document"]["score"] + '</p>' + '</div>' + '<div class="tweet-txt col-xs-12">' + tweet['text'] + '</div>')
+
+
 
 
                 watsonScore = result["sentiment"]["document"]["score"]
@@ -114,7 +122,12 @@ $.ajax({
                     tweetdate: tweet['created_at'].slice(0, 10),
                     score: watsonScore
                 }
-                console.log("about to return");
+                printNumberofRequests()
+                function printNumberofRequests() {
+                  myprintNumberofRequestCalls += 1
+                  // append to overall watson score box
+                  $('#total-tweets-filtered').html('<p>' + myprintNumberofRequestCalls + '</p>')
+                }
                 return ultimateWatsonObj
             }).catch(error => {
 
@@ -228,9 +241,13 @@ myChartWats = new Chart(watsChart, {
     options: {
         scales: {
             yAxes: [{
+              scaleLabel: {
+                  display: true,
+                  labelString: 'Sentiment',
                 ticks: {
                     beginAtZero: true
                 }
+              }
             }]
         }
     }
