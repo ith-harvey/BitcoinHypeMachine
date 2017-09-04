e00b631edcca0db6ad801f931484d38d24cc0dd6
Ian Harvey <ith.harvey@gmail.com>
2017-09-04 18:32:55 -0500

    created scrolling box

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,10 @@ var db = require('../db')
 router.get('/', (req,res,next) => {
   db('tweets').select('*').then( tweets => {
     db('btc_prices').select('*').then( btc_prices => {
-      console.log('thing we need',tweets[0]);
       tweets = tweets.map( tweet => {
         tweet.watson_score = Number(tweet.watson_score).toFixed(2)
         return tweet
       })
-      console.log('thing we need',tweets[0]);
       res.render('index', {tweets, btc_prices});
     })
   }).catch( error => {
