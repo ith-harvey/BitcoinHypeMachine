06efdf403f3676d7338a3148704f846cb2ad7951
Ian Harvey <ith.harvey@gmail.com>
2017-09-07 17:41:07 -0500

    completed port change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8000
+const port = process.env.PORT || '8000'
 const hbs = require('hbs')
 const apiRequest = require('./api-request/index.js')
 const promiseVariables = require('./api-request/promise-variables.js')
